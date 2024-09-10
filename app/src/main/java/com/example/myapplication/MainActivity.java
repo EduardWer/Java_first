3e184c0f0a3d4465080bbb8d16990e97e0f29379
Eduard_wer <eduard.unanov66@mail.ru>
2024-09-10 15:08:53 +0300

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView animationIV;
+    private Button startBtn;
+    private Button pauseBtn;
+    private AnimationDrawable frameAnimation;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        animationIV = findViewById(R.id.animImageView);
+        startBtn = findViewById(R.id.startButton);
+        pauseBtn = findViewById(R.id.pauseButton);
+
+        frameAnimation = (AnimationDrawable) animationIV.getDrawable();
+
+        startBtn.setOnClickListener(v -> {
+            if (! frameAnimation.isRunning()) {
+                frameAnimation.start();
+            }
+        });
+
+        pauseBtn.setOnClickListener( v-> {
+            if (frameAnimation.isRunning()) {
+
+                frameAnimation.stop();
+            }
+        });
+
+
+
+
+    }
+}
